03ad90c73a2ba1bfe74cc4866ca94ca9d271b0b8
CubiRubick <Jose_Guzman_Villegas@outlook.com>
2022-12-12 23:54:39 -0600

    add model in admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    class Meta:
+        model = User
+
+admin.site.register(User, UserAdmin)
